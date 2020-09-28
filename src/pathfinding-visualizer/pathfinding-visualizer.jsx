d080fba15bd7b1cd6a1ac642e9591b4c153e77c5
Brandon Lai <brandon.lai@vanderbilt.edu>
2020-09-27 23:47:05 -0400

    sets up intial structure of project; starts pathfinder

---
+++ src/pathfinding-visualizer/pathfinding-visualizer.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+
+import './PathfindingVisualizer.css';
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
